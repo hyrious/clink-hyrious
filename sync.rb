5d5e22f0bee3d7855d1545f82d984ab5b3a61598
hyrious <hyrious@outlook.com>
2026-01-31 15:05:52 +0800

    fix sync script

--- sync.rb
+++ sync.rb
@@ -4,9 +4,9 @@
 require "open-uri"
 
 # Setup 'z' command.
-code = URI.open('https://raw.gitmirror.com/shunsambongi/clink-zoxide/refs/heads/master/zoxide.lua', &:read)
+code = URI.open('https://raw.githubusercontent.com/shunsambongi/clink-zoxide/refs/heads/master/zoxide.lua', &:read)
 IO.write 'zoxide.lua', code
 
 # Setup vscode shell integration.
-code = URI.open('https://raw.gitmirror.com/chrisant996/clink-gizmos/refs/heads/main/vscode_shell_integration.lua', &:read)
+code = URI.open('https://raw.githubusercontent.com/chrisant996/clink-gizmos/refs/heads/main/vscode_shell_integration.lua', &:read)
 IO.write 'vscode_shell_integration.lua', code
